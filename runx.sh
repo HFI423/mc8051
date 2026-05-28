d148680a971e8d03188bb2fc3e7be70bcb353cf0
Lennart Heinrich <lennart.heinrich@edu.fhdw.de>
2026-05-28 23:28:44 +0200

    add script: run extended

---
+++ runx.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -e
+
+if [ "$#" -lt 1 ]; then
+    echo "Usage: ./runx.sh PROGRAM_FILE_IN_MSIM_WITHOUT_EXTENSION [STOP_TIME, default 1000ns]"
+    echo "Example: ./run.sh minitest_fp 1ms"
+    exit 1
+fi
+
+cd msim
+./hex2dual $1.h51
+\cp $1.dua ../vhdl/mc8051_rom.dua
+cd ..
+./run.sh tb_mc8051_top ${2:-10us}
